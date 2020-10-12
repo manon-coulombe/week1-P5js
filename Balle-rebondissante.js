25d5bc53e99da9f6487e3dff23395ce20fd8c381
JojoDz <72552227+JojoDz@users.noreply.github.com>
2020-10-12 15:19:07 +0200

    Create Balle-rebondissante.js

---
+++ Balle-rebondissante.js
@@ -0,0 +1,27 @@
+//variable : modif au clic
+let XA = 200;
+let YA = 400;
+let dim = 30.0;
+let direction = true
+// Mise en place du code
+function setup() {
+  createCanvas(500, 480);
+  background(0);
+}
+function draw(){
+  background(0);
+  ellipse(XA,YA,dim,dim);
+  if (mouseIsPressed) {
+    if (direction === true) {
+    XA +=5;
+    YA -=3;
+   } else {
+      XA -=5;
+    YA +=3;
+   } if (XA >= 500) {
+     console.log("J'ai dépassé")
+     direction = false;
+   }
+  }
+  console.log(XA,YA);
+}
